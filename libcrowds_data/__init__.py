68271555ef3226039b91abb65677eda834faa9d8
alexandermendes <alexanderhmendes@gmail.com>
2016-06-09 23:23:56 +0100

    Version bump

--- libcrowds_data/__init__.py
+++ libcrowds_data/__init__.py
@@ -11,7 +11,7 @@ from flask import current_app as app
 from flask.ext.plugins import Plugin
 
 __plugin__ = "LibCrowdsData"
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 
 
 class LibCrowdsData(Plugin):
