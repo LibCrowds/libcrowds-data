ce443b03ca919e799566751d13a3d357299d38c2
alexandermendes <alexanderhmendes@gmail.com>
2016-11-10 15:37:41 +0000

    Use relative import for default settings

--- libcrowds_data/__init__.py
+++ libcrowds_data/__init__.py
@@ -8,7 +8,7 @@ Global data repository page for LibCrowds.
 
 import os
 import json
-import default_settings
+from . import default_settings
 from flask import current_app as app
 from flask.ext.plugins import Plugin
 
